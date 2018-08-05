c040eb6bcb38a92d5713ef714c16a51a433bd71f
Roger-Hong <federer.hyj2721@gmail.com>
2018-08-04 22:58:31 -0700

    3

--- contract/PubFree.sol
+++ contract/PubFree.sol
@@ -11,10 +11,12 @@ contract PubFree is Ownable {
         address account;
         uint balance;
         uint reputation;
-        // Mapping of purchased books.
-        mapping(uint => bool) purchasedBooks;
-        // Mapping of published books.
-        mapping(uint => bool) publisedBooks;
+        // Mapping from the idx of books purchased by the user to book ID.
+        mapping(uint => uint) purchasedBooks;
+        uint purchasedBookCount;
+        // Mapping from the idx of books published by the user to book ID.
+        mapping(uint => uint) publisedBooks;
+        uint publishedBooksCount;
     }
     
     mapping(uint => User) public users;
@@ -26,12 +28,7 @@ contract PubFree is Ownable {
     function newUser(string name) external returns (uint) {
         uint newUserId = userCount;
         userCount = userCount.add(1);
-        users[newUserId] = User({
-            name: name,
-            account: msg.sender,
-            balance: 0,
-            reputation: 0
-        });
+        users[newUserId] = User({name: name, account: msg.sender, balance: 0, reputation: 0, purchasedBookCount: 0, publishedBooksCount:0});
         userAccountToId[msg.sender] = newUserId;
         return newUserId;
     }
@@ -42,5 +39,19 @@ contract PubFree is Ownable {
         users[userId].balance += newBalance - oldBalance;
         oldBalance = newBalance;
         return newBalance;
+    }
+    
+    function getUserInfo() external returns (string name, address account, uint balance, uint reputation, uint[], uint[]) {
+        uint userId = userAccountToId[msg.sender];
+        User user = users[userId];
+        uint[] purchased;
+        uint[] published;
+        for (uint idx1 = 0; idx1 < user.purchasedBookCount; idx1++) {
+            purchased.push(user.purchasedBooks[idx1]);
+        }
+        for (uint idx2 = 0; idx2 < user.publishedBooksCount; idx2++) {
+            published.push(user.publisedBooks[idx2]);
+        }
+        return (user.name, user.account, user.balance, user.reputation, purchased, published);
     }
 }
